2f206006bab77113509361d51c7cdd2234bd1d5c
João Lee <joaumlimaum@gmail.com>
2024-11-08 11:30:33 -0300

    Exercício concluído

---
+++ automatizando-o-bug-hunting-com-python/exercicio-1.py
@@ -0,0 +1,15 @@
+# Exercício 1
+# A função sum_two_numbers abaixo contém um bug. Crie um exemplo na docstring que pega esse bug ao rodar o módulo doctest e, em seguida, corrija-o.
+
+def sum_two_numbers(a, b):
+    """Retorna a soma de dois números recebidos por parâmetro.
+
+    Exemplos
+    --------
+    >>> sum_two_numbers(0, 0)
+    0
+    >>> sum_two_numbers(2, 4)
+    6
+    """
+
+    return a + b
